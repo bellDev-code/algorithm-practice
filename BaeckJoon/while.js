8d5dbbb104fea99e9b81d8d68da1bef622d097c2
bellDev-code <sumaoo20@naver.com>
2020-10-30 16:00:44 +0900

    algorithm

--- BaeckJoon/while.js
+++ BaeckJoon/while.js
@@ -1,1 +1,31 @@
 // 1. 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+function solution(A, B) {
+  while (A > 0 && B < 10) {
+    const result = A + B;
+    // console.log(result);
+    break;
+  }
+}
+
+// solution(1, 1);
+// solution(2, 3);
+// solution(3, 4);
+// solution(9, 8);
+// solution(5, 2);
+// solution(0, 0);
+
+// 2. 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+function solution(A, B) {
+  while (A > 0 && B < 10) {
+    const plus = A + B;
+    // console.log(plus);
+    break;
+  }
+}
+
+// solution(1, 1);
+// solution(2, 3);
+// solution(3, 4);
+// solution(9, 8);
+// solution(5, 2);
