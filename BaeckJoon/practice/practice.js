23885aa90f9a39b5a1c6a94672af8ad3d9469da1
bellDev-code <sumaoo20@naver.com>
2020-12-21 15:39:24 +0900

    function practice

---
+++ BaeckJoon/practice/practice.js
@@ -0,0 +1,18 @@
+// split 함수
+
+// const str = "The quick brown fox jumps over the lazy dog.";
+
+// const word = str.split(" ");
+
+// console.log(word);
+// console.log(word[0]);
+
+// sort 함수
+// const array = ["banana", "watermelon", "cherry", "melon"];
+// array.sort().join();
+// console.log(array);
+
+// frrom 함수
+// console.log(Array.from("foo"));
+
+// console.log(Array.from([1, 2, 3], (x) => x + x));
