93301b48072f8d56ae2d5f73020006a8beb7d474
bellDev-code <sumaoo20@naver.com>
2021-01-12 21:26:07 +0900

    분수 찾기 algorithm

--- BaeckJoon/practice/practiceMath.js
+++ BaeckJoon/practice/practiceMath.js
@@ -19,14 +19,41 @@ breakEvenFuc(1000, 70, 170);
 
 function hexagon(N) {
   // 처음에 방이 1개, 두 번째 범위에서 방이 7개, 세 번째 범위에서 방이 19개, 네 번째 범위에서 37개
-  // 겉에 있는 방의 개수는 6개씩 늘어난다. (6, 12, 18 ...)
+  // 1번 방 이후 감싸고 있는 방의 개수는 6개씩 늘어난다. (6, 12, 18 ...)
   let counter = 1;
   let range = 1;
 
   while (range < N) {
     range += counter++ * 6;
   }
-  console.log(counter);
+  // console.log(counter);
 }
 
 hexagon(58);
+
+// 분수 찾기
+
+// 규칙은 [1/1], [2/1, 1/2], [3/1, 2/2, 1/3], [4/1, 3/2, 2/3, 1/4] ...
+// 분자는 내림차순, 분모는 오름차순
+function fractionalFunc(X) {
+  // counter = 해당 그룹의 분수의 갯수를 말한다.
+  let counter = 0;
+
+  // 1/1부터 시작한다고 생각할 때, 해당 그룹의 분수 개수는 1부터 1씩 증가한다.
+  // 그룹 개수가 증가할 때마다 입력 받은 X에서 그룹을 빼주면 해당 그룹에 도달했을 때, X는 0 또는 음수가 된다.
+  while (X > 0) {
+    counter++;
+    X = X - counter;
+  }
+  console.log(X, counter);
+
+  // 분모는 짝수 일때, counter +1 => 오름차순
+  // 분자는 짝수 일때, 1 + (-X) => 내림차순
+  if (counter % 2 === 0) {
+    console.log(`${counter + X}/${1 + -X}`);
+  } else {
+    console.log(`${1 + -X}/${counter + X}`);
+  }
+}
+
+fractionalFunc(14);
