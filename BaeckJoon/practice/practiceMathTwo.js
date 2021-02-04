aaaf931b416f7c32e5eae9957effb4c78a136d2c
bellDev-code <sumaoo20@naver.com>
2021-02-04 13:23:06 +0900

    소수찾기 2

--- BaeckJoon/practice/practiceMathTwo.js
+++ BaeckJoon/practice/practiceMathTwo.js
@@ -1,10 +1,45 @@
 "use-strict";
 
-const minorityFunc = () => {
-  const input = "1, 3, 5, 7";
-  console.log(input);
-  const numbers = input.split(" ").map((num) => parseInt(num));
-  console.log(numbers);
-};
+const input = ["4", "1 3 5 7"];
+const num = parseInt(input.shift());
+const numbers = input
+  .shift()
+  .split(" ")
+  .map((num) => parseInt(num));
+let counter = 0;
+
+function minority(n) {
+  if (n < 2) {
+    return;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return;
+    }
+  }
+  counter++;
+}
+
+for (let i = 0; i < num; i++) {
+  minority(numbers[i]);
+}
+console.log(counter);
 
 minorityFunc();
+
+const range = ["64", "65"];
+const primeNumArr = [];
+let primeNumSum = 0;
+
+function rangeMinority(n) {
+  if (n < 2) {
+    return;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return;
+    }
+  }
+}
